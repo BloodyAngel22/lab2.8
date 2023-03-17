a9472d65a2624af84ec3aa17d793ce661ac795c1
BloodyAngel22 <NanoViper222@yandex.ru>
2023-03-17 18:38:21 +0300

    Добавьте файлы проекта.

---
+++ lab2.8/Source.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+struct RaggedArray {
+	int** data = NULL;
+	int rows = 0;
+	int* cols = NULL;
+	int* tmp = NULL;
+};
+
+int main() {
+	system("chcp 1251");
+
+	RaggedArray mas;
+
+	mas.data = (int**)malloc(sizeof(int*) * mas.rows);
+	mas.cols = (int*)malloc(sizeof(int));
+	int limiter = 999999;
+
+	printf("rows = "); scanf_s("%d", &mas.rows);
+	for (int i = 0; i < mas.rows; i++) {
+	int counter=0;
+	mas.tmp = (int*)malloc(sizeof(int) * limiter);
+	printf("¬ведите элементы массива\n");
+		for (int j = 0; j < limiter; j++) {
+			scanf_s("%d", &mas.tmp[j]);
+			if (mas.tmp[j] == '\0') break;
+			counter++;
+		}
+		mas.cols[i] = counter;
+		mas.data[i] = (int*)malloc(sizeof(int) * mas.cols[i]);
+		for (int j = 0; j < mas.cols[i]; j++) {
+			mas.data[i][j] = mas.tmp[j];
+		}
+	}
+
+	for (int i = 0; i < mas.rows; i++) {
+		for (int j = 0; j < mas.cols[i]; j++) {
+			printf("%d ", mas.data[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < mas.rows; i++)
+		free(mas.data[i]);
+	free(mas.data);
+	free(mas.cols);
+	free(mas.tmp);
+}
